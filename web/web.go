2d7795bbcd62c9b694dac70581d74020ad0e8de4
WinPooh32 <winpooh32@yandex.ru>
2019-02-26 13:22:43 +1000

    Add db extraction example code

--- web/web.go
+++ web/web.go
@@ -3,14 +3,21 @@ package web
 import (
 	"net/http"
 
+	"github.com/WinPooh32/feedflow/database"
+
 	gintemplate "github.com/foolin/gin-template"
 	"github.com/gin-gonic/gin"
 )
 
 func index(ctx *gin.Context) {
-	gintemplate.HTML(ctx, http.StatusOK, "index", gin.H{
-		"title": "Hello, web!",
-	})
+	if _, ok := database.Extract(ctx); ok {
+
+		gintemplate.HTML(ctx, http.StatusOK, "index", gin.H{
+			"title": "Hello, web!",
+		})
+	} else {
+		ctx.Writer.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func notFound(ctx *gin.Context) {
