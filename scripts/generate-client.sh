4bdc39696c712d4c6a601ac728623979026ba253
WinPooh32 <winpooh32@yandex.ru>
2019-03-07 13:36:10 +1000

    Fix code generators scripts

--- scripts/generate-client.sh
+++ scripts/generate-client.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #https://github.com/OpenAPITools/openapi-generator
-openapi-generator generate -i api-oas3.yaml -g typescript-axios -o ../client-ts-axios
+openapi-generator generate -i ../api-oas3.yaml -g typescript-axios -o ./client-ts-axios
--- scripts/generate-server.sh
+++ scripts/generate-server.sh
@@ -2,4 +2,4 @@
 
 export GO_POST_PROCESS_FILE="/usr/bin/gofmt -w"
 
-openapi-generator generate -i api-oas3.yaml -g go-gin-server -o ../server-go-gin
+openapi-generator generate -i ../api-oas3.yaml -g go-gin-server -o ./server-go-gin
